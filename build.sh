03126b8b639c9b5cbfeab567b6288271f5ad729f
Anil Sharma <anil@collager.com>
2017-02-08 20:43:47 -0800

    Web App using Trillo Server and a sample microservice.

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Builds trillo-server and deploys it to bin dir
-rm -rf bin
-cd trillo-core
-mvn install
-cd ../trillo-security
-mvn install
-cd ../trillo-server
+rm -rf microsvc-bin
+
+cd microservice
 mvn install
 
-cp application.yml ../bin
-cp startTrilloServer.txt ../bin/startTrilloServer.sh
+cp startMicroService.txt ../microsvc-bin/startMicroService.sh
---
+++ microservice/startMicroService.txt
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Copied over to ../bin as ".sh" file by "../build.sh"
+# Starts example micro service
+
+java -jar trillo-microservice-sample.jar --server.port=8090  --database.dir=../database
